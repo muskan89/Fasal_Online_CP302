45d4a930e0f7d10478db92517c7e7d11b3c80904
Ashish Sulania <81580164+aks-2@users.noreply.github.com>
2022-10-16 21:19:07 +0530

    sign up page added

---
+++ onlinefasal/lib/sign_up.dart
@@ -0,0 +1,146 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+
+class SignUp extends StatefulWidget {
+  const SignUp({Key? key}) : super(key: key);
+
+  @override
+  State<SignUp> createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(10),
+        child: ListView(
+          children: <Widget>[
+            Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(10),
+                child:
+                    Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  Image.asset('assets/images/logo.png'),
+                  const Text(
+                    'Fasal Online',
+                    style: TextStyle(
+                        color: Color(0xFF00c292),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30),
+                  ),
+                ])),
+            Container(
+              alignment: Alignment.center,
+              padding: const EdgeInsets.all(10),
+              child: const Text(
+                'Sign Up',
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(5),
+              child: TextField(
+                controller: nameController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'name',
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(5),
+              child: TextField(
+                obscureText: true,
+                controller: passwordController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'mobile number',
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(5),
+              child: TextField(
+                obscureText: true,
+                controller: passwordController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'state',
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(5),
+              child: TextField(
+                obscureText: true,
+                controller: passwordController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'email',
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(5),
+              child: TextField(
+                obscureText: true,
+                controller: passwordController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'username',
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(5),
+              child: TextField(
+                obscureText: true,
+                controller: passwordController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'password',
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(5),
+              child: TextField(
+                obscureText: true,
+                controller: passwordController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'your total land',
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(5),
+              child: TextField(
+                obscureText: true,
+                controller: passwordController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'crops you want to grow',
+                ),
+              ),
+            ),
+            Container(
+              height: 50,
+              padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+              child: ElevatedButton(
+                child: const Text('Sign Up'),
+                onPressed: () {
+                  log(nameController.text);
+                  log(passwordController.text);
+                  log('Signed Up Successful!!');
+                },
+              ),
+            ),
+          ],
+        ));
+  }
+}
